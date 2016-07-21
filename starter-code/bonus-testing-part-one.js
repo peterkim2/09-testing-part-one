75989c6b17eebd7e4cd32795d463ec3371382a59
Rick Patci <rpatci@gmail.com>
2016-07-21 15:54:30 -0700

    update TODOs

--- starter-code/bonus-testing-part-one.js
+++ starter-code/bonus-testing-part-one.js
@@ -21,7 +21,8 @@ function expect(expression, failureMessage, successMessage) {
   console.log('test passed:', successMessage);
 }
 
-/* Below is an example of our test in action. Run this file in node
+/* Below is an example of our test in action.
+TODO: Run this file in node
     to see what happens when the test fails, then change `ricksFaveAnimal`
     to get the test to pass!
 */
@@ -46,17 +47,14 @@ var favoriteAnimals = ['elephants', 'penguins', 'eagles', 'camels'];
 var nextAnimal;
 
   /* TODO:
-      Assign one of your favorite animals dynamically by chance to the 
+      Assign one of your favorite animals dynamically by chance to the
       nextAnimal variable   :-)
       Your code begins on the next line: */
 
   /* TODO:
       Write a test! Use the `expect()` function we defined earlier to ensure
       that an element in the favoriteAnimals array was assigned to nextAnimal.
-      No hard-coded results allowed! (ex: seeing if nextAnimal is equal to 
+      No hard-coded results allowed! (ex: seeing if nextAnimal is equal to
       just 'penguin').
       Remember to: pass in your expression, and write a failure and a success
       message. Your test begins on the next line: */
-
-
-
