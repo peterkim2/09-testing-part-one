05bb91811bb02ecf5402583d7747e1e5defa2633
Rick Patci <rpatci@gmail.com>
2016-10-07 07:05:36 -0700

    export assert as vendor lib

--- starter-code/bonus-testing-part-one.js
+++ starter-code/bonus-testing-part-one.js
@@ -1,33 +1,33 @@
-/* Welcome to an intro to testing! This practice module
-    will allow you to become familiar with testing concepts.
-    We test to check the I/O of our apps. That is - we are not
+var assert = require('./vendor/assert');
+
+/* Welcome to an intro to testing! This exercise
+    will help you to become familiar with testing concepts.
+    We test to check the output of our apps. That is - we are not
     checking for HOW something works, but the end RESULT and
-    state of a particular place in our application.
-*/
+    state of a particular place in our application.*/
 
+/* The 'assert' that we are loading in at the top is our test library.
+    This library uses one function, called 'assert',
+    which accepts three arguments:
+    1. An expression to check whether true or false,
+    2. A success message string if true.
+    3. A failing message string if false.
 
-/* Below is a basic example of a test "framework" where
-    our test is a function that we can reuse to check against
-    different components of our application.
-    We test for failing functionality first to then refactor
-    our code to pass.
-
-    This is common practice in testing ('red-green' refactoring).
-*/
-
-function assert(expression, successMessage, failureMessage) {
-  var green = '  \x1b[32m(test passing):\x1b[m';
-  var red = '  \x1b[31m(test failing):\x1b[0m';
-  var test = '\x1b[35m' + assert.caller.name + '\x1b[m';
-  if (expression) {
-    return console.log(test,'\n', green, successMessage);
-  }
-  console.log(test, '\n', red, failureMessage);
-}
+    Common practice in testing is to write a test, ensure
+      that it fails FIRST - for the right reason! - and then write
+      just the minimum amount of code to make the test pass
+      ('red-green refactoring' is the jargon term for this). */
 
 //  Below, we will write an example of our test in action:
 
 var ricksFaveAnimal;
+
+function testRicksFaveAnimal() {
+  assert (true, 'hah', 'wut');
+}
+
+testRicksFaveAnimal();
+
 
 /* ========================================================================
 ------------------------- Favorite Animals --------------------------------
@@ -39,22 +39,23 @@ animals. You only have time for one. How do you choose just one?!
 var favoriteAnimals = ['elephants', 'penguins', 'eagles', 'camels'];
 var nextAnimal;
 
-
-/* TODO:
-Write a test FIRST! Use the `assert()` function below to ensure
+/* NOTE:
+Write a test FIRST! Use the 'assert()' function below to ensure
 that an element in the favoriteAnimals array was assigned to nextAnimal.
 
 No hard-coded results allowed! (e.g. -  seeing if nextAnimal is equal to
 just 'penguin').
+
 Remember to: pass in your expression, and write a success and a failure
 message. */
 
-function faveAnimalTest() {
-  assert(
-
-  );
+function testWhichAnimal() {
+  // TODO: Complete this assert function.
+  // Don't forget your three arguments!
+  assert();
 };
 
+testWhichAnimal();
 /* TODO:
 Now assign one of your favorite animals dynamically, by chance, to the
 nextAnimal variable ... then invoke your test!   :-)
